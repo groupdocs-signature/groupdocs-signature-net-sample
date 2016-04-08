dd7a81c2b8337fe20e23953b8a0d13926abcc915
vadim-gurin-groupdocs <vadim.gurin@groupdocs.com>
2016-04-08 16:33:46 +0300

    SIGNATURENET-2237 Implement Web UI for new Signature product preview
    Implemented dynamic generation of page image URLs and document
    descriptions for PDF documents.

--- MVCDemo/Controllers/SignatureController.cs
+++ MVCDemo/Controllers/SignatureController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using System.Web.Script.Serialization;
 using System.Web.UI;
 using Aspose.Pdf;
@@ -17,7 +19,7 @@ namespace MVCDemo.Controllers
     public class SignatureController : Controller
     {
         [HttpPost]
-        public ActionResult PublicGetDocument()
+        public ActionResult PublicGetDocument(string documentGuid, string recipientGuid)
         {
             var result = new
             {
@@ -26,15 +28,15 @@ namespace MVCDemo.Controllers
                 {
                     document = new
                     {
-                        guid = "cea6784811dc54d7feac5fcb5ef8817a",
-                        name = "sample.pdf",
-                        signedName = "sample_signed1.pdf",
+                        guid = documentGuid,
+                        name = documentGuid,
+                        signedName = documentGuid + "_signed1.pdf",
                         signedFromAll = false,
                         recipient = new
                         {
                             id = 0,
                             guid = "71d1f3ef88a5d7fe32f4c46588a69887",
-                            documentGuid = "cea6784811dc54d7feac5fcb5ef8817a",
+                            documentGuid,
                             firstName = "",
                             lastName = "",
                             email = "",
@@ -112,7 +114,49 @@ namespace MVCDemo.Controllers
         {
             string fileNameExtension = Path.GetExtension(path).TrimStart('.');
             fileNameExtension = fileNameExtension.ToLower();
-            fileNameExtension = fileNameExtension.Substring(0, 1).ToUpper() + fileNameExtension.Substring(1);
+            string fileType = fileNameExtension.Substring(0, 1).ToUpper() + fileNameExtension.Substring(1);
+
+            string appDataPath = Server.MapPath("~/App_Data/");
+            string fullPathToDocument = Path.Combine(appDataPath, path);
+            int pageCount= 1;
+            PageDescription[] pageDescs = null;
+            int maxPageHeight = 0, widthForMaxHeight = 0;
+            int pageHeight;
+            bool isFirstPass = true;
+            switch (fileNameExtension)
+            {
+                case "pdf":
+                    Aspose.Pdf.Document document = new Document(fullPathToDocument);
+                    JpegDevice jpegDevice = new JpegDevice(quality);
+                    pageCount = document.Pages.Count;
+                    const int asposePdfTrialPagesLimit = 4;
+                    if (pageCount > asposePdfTrialPagesLimit)
+                        pageCount = asposePdfTrialPagesLimit;
+                    pageDescs = new PageDescription[pageCount];
+                    for (int i = 0; i < pageDescs.Length; i++)
+                    {
+                        var page = document.Pages[i + 1];
+                        pageHeight = (int) page.Rect.Height;
+                        pageDescs[i] = new PageDescription() {
+                            w = (int)page.Rect.Width,
+                            h = pageHeight,
+                            pageNumber = i
+                        };
+                        if (isFirstPass || pageHeight > maxPageHeight)
+                        {
+                            maxPageHeight = pageHeight;
+                            isFirstPass = false;
+                        }
+                    }
+                    break;
+            }
+            string[] pageImageUrls = GetImageUrls(path, 0, pageCount, width, quality);
+            string documentDescription = new JavaScriptSerializer().Serialize(new
+            {
+                pages = pageDescs,
+                maxPageHeight,
+                widthForMaxHeight
+            });
             var result = new
             {
                 path = path,
@@ -120,15 +164,13 @@ namespace MVCDemo.Controllers
                 fileType = fileNameExtension,
                 url = "http://localhost:13469/gd-signature/signature2/GetFileHandler?path=sample.pdf\u0026getPdf=false\u0026useHtmlBasedEngine=false\u0026supportPageRotation=false",
                 pdfDownloadUrl = "http://localhost:13469/gd-signature/signature2/GetFileHandler?path=sample.pdf\u0026getPdf=true\u0026useHtmlBasedEngine=false\u0026supportPageRotation=false",
-                name = "sample.pdf",
-                imageUrls= new[] { "http://localhost:13469/gd-signature/signature2/GetDocumentPageImageHandler?path=sample.pdf\u0026width=395\u0026quality=80\u0026usePdf=False\u0026useHtmlBasedEngine=False\u0026rotate=False\u0026pageIndex=0",
-                "http://localhost:13469/gd-signature/signature2/GetDocumentPageImageHandler?path=sample.pdf\u0026width=395\u0026quality=80\u0026usePdf=False\u0026useHtmlBasedEngine=False\u0026rotate=False\u0026pageIndex=1",
-                "http://localhost:13469/gd-signature/signature2/GetDocumentPageImageHandler?path=sample.pdf\u0026width=395\u0026quality=80\u0026usePdf=False\u0026useHtmlBasedEngine=False\u0026rotate=False\u0026pageIndex=2"},
+                name = path,
+                imageUrls= pageImageUrls,
                 lic = true,
                 pdfPrintUrl = "http://localhost:13469/gd-signature/signature2/GetPdfWithPrintDialogHandler?path=sample.pdf\u0026useHtmlBasedEngine=false\u0026supportPageRotation=false",
                 pageHtml = (object)null,
                 pageCss = (object)null,
-                documentDescription = "{\"pages\":[{\"w\":612,\"h\":936,\"number\":1},{\"w\":612,\"h\":936,\"number\":2},{\"w\":612,\"h\":936,\"number\":3},{\"w\":612,\"h\":936,\"number\":4},{\"w\":612,\"h\":936,\"number\":5},{\"w\":612,\"h\":936,\"number\":6},{\"w\":612,\"h\":936,\"number\":7},{\"w\":612,\"h\":936,\"number\":8},{\"w\":612,\"h\":936,\"number\":9},{\"w\":612,\"h\":936,\"number\":10},{\"w\":612,\"h\":936,\"number\":11},{\"w\":612,\"h\":936,\"number\":12},{\"w\":612,\"h\":936,\"number\":13},{\"w\":612,\"h\":936,\"number\":14},{\"w\":612,\"h\":936,\"number\":15},{\"w\":612,\"h\":936,\"number\":16},{\"w\":612,\"h\":936,\"number\":17},{\"w\":612,\"h\":936,\"number\":18},{\"w\":612,\"h\":936,\"number\":19},{\"w\":612,\"h\":936,\"number\":20},{\"w\":612,\"h\":936,\"number\":21},{\"w\":612,\"h\":936,\"number\":22},{\"w\":612,\"h\":936,\"number\":23},{\"w\":612,\"h\":936,\"number\":24},{\"w\":612,\"h\":936,\"number\":25},{\"w\":612,\"h\":936,\"number\":26},{\"w\":612,\"h\":936,\"number\":27},{\"w\":612,\"h\":936,\"number\":28},{\"w\":612,\"h\":936,\"number\":29},{\"w\":612,\"h\":936,\"number\":30},{\"w\":612,\"h\":936,\"number\":31},{\"w\":612,\"h\":936,\"number\":32},{\"w\":612,\"h\":936,\"number\":33},{\"w\":612,\"h\":936,\"number\":34},{\"w\":612,\"h\":936,\"number\":35},{\"w\":612,\"h\":936,\"number\":36},{\"w\":612,\"h\":936,\"number\":37},{\"w\":612,\"h\":936,\"number\":38},{\"w\":612,\"h\":936,\"number\":39},{\"w\":612,\"h\":936,\"number\":40},{\"w\":612,\"h\":936,\"number\":41},{\"w\":612,\"h\":936,\"number\":42},{\"w\":612,\"h\":936,\"number\":43},{\"w\":612,\"h\":936,\"number\":44},{\"w\":612,\"h\":936,\"number\":45},{\"w\":612,\"h\":936,\"number\":46},{\"w\":612,\"h\":936,\"number\":47},{\"w\":612,\"h\":936,\"number\":48},{\"w\":612,\"h\":936,\"number\":49},{\"w\":612,\"h\":936,\"number\":50},{\"w\":612,\"h\":936,\"number\":51},{\"w\":612,\"h\":936,\"number\":52},{\"w\":612,\"h\":936,\"number\":53},{\"w\":612,\"h\":936,\"number\":54},{\"w\":612,\"h\":936,\"number\":55},{\"w\":612,\"h\":936,\"number\":56},{\"w\":612,\"h\":936,\"number\":57},{\"w\":612,\"h\":936,\"number\":58},{\"w\":612,\"h\":936,\"number\":59},{\"w\":612,\"h\":936,\"number\":60},{\"w\":612,\"h\":936,\"number\":61},{\"w\":612,\"h\":936,\"number\":62},{\"w\":612,\"h\":936,\"number\":63},{\"w\":612,\"h\":936,\"number\":64},{\"w\":612,\"h\":936,\"number\":65},{\"w\":612,\"h\":936,\"number\":66},{\"w\":612,\"h\":936,\"number\":67},{\"w\":612,\"h\":936,\"number\":68},{\"w\":612,\"h\":936,\"number\":69},{\"w\":612,\"h\":936,\"number\":70},{\"w\":612,\"h\":936,\"number\":71},{\"w\":612,\"h\":936,\"number\":72},{\"w\":612,\"h\":936,\"number\":73},{\"w\":612,\"h\":936,\"number\":74},{\"w\":612,\"h\":936,\"number\":75},{\"w\":612,\"h\":936,\"number\":76},{\"w\":612,\"h\":936,\"number\":77},{\"w\":612,\"h\":936,\"number\":78},{\"w\":612,\"h\":936,\"number\":79},{\"w\":612,\"h\":936,\"number\":80},{\"w\":612,\"h\":936,\"number\":81},{\"w\":612,\"h\":936,\"number\":82},{\"w\":612,\"h\":936,\"number\":83},{\"w\":612,\"h\":936,\"number\":84},{\"w\":612,\"h\":936,\"number\":85},{\"w\":612,\"h\":936,\"number\":86},{\"w\":612,\"h\":936,\"number\":87},{\"w\":612,\"h\":936,\"number\":88},{\"w\":612,\"h\":936,\"number\":89},{\"w\":612,\"h\":936,\"number\":90},{\"w\":612,\"h\":936,\"number\":91},{\"w\":612,\"h\":936,\"number\":92},{\"w\":612,\"h\":936,\"number\":93},{\"w\":612,\"h\":936,\"number\":94},{\"w\":612,\"h\":936,\"number\":95},{\"w\":612,\"h\":936,\"number\":96},{\"w\":612,\"h\":936,\"number\":97},{\"w\":612,\"h\":936,\"number\":98},{\"w\":612,\"h\":936,\"number\":99},{\"w\":612,\"h\":936,\"number\":100},{\"w\":612,\"h\":936,\"number\":101},{\"w\":612,\"h\":936,\"number\":102},{\"w\":612,\"h\":936,\"number\":103},{\"w\":612,\"h\":936,\"number\":104},{\"w\":612,\"h\":936,\"number\":105}],\"maxPageHeight\":936,\"widthForMaxHeight\":612}",
+                documentDescription,
                 urlForResourcesInHtml = (object)null,
                 sharedCss = (object)null,
                 success = true
@@ -181,6 +223,45 @@ namespace MVCDemo.Controllers
                 return File(pageImageFullPath, "image/jpeg");
             else
                 return new EmptyResult();
+        }
+
+
+        public ActionResult PublicSignDocument(string documentGuid, string documentId)
+        {
+            return null;
+        }
+
+        private string[] GetImageUrls(string path, int startingPageNumber, int pageCount, int? pageWidth, int? quality)
+        {
+            RouteValueDictionary routeValueDictionary = new RouteValueDictionary()
+            {
+                {"path", path},
+                {"width", pageWidth},
+                {"quality", quality},
+            };
+
+            string[] pageUrls = new string[pageCount];
+            for (int i = 0; i < pageCount; i++)
+            {
+                routeValueDictionary["pageIndex"] = startingPageNumber + i;
+                pageUrls[i] = ConvertUrlToAbsolute(new UrlHelper(Request.RequestContext).Action("GetDocumentPageImage", routeValueDictionary));
+            }
+            return pageUrls;
+        }
+
+
+        private string ConvertUrlToAbsolute(string inputUrl)
+        {
+            HttpRequestBase request = Request;
+            string applicationHost = String.Format("{0}://{1}{2}", request.Url.Scheme, request.Url.Host,
+                                                    request.Url.Port == 80 ? String.Empty : ":" + request.Url.Port);
+
+            if (applicationHost.EndsWith("/"))
+            {
+                applicationHost = applicationHost.Substring(0, applicationHost.Length - 1);
+            }
+            string result = string.Format("{0}{1}", applicationHost, inputUrl);
+            return result;
         }
     }
 }
